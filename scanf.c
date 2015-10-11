a8064266d3b0289e04bdaf4ba165e31142d4a885
Bob Zheng <leavenotrace@foxmail.com>
2015-10-11 22:50:31 +0800

    Create scanf.c

---
+++ scanf.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    float r,s,c;
+    
+    scanf("%f",&r);
+    
+    printf("r:%.2f\n",r);
+    
+    
+    s=r*r*3.14;
+          
+          c=2*3.14*r;
+    
+    printf("s:%.2f  c:%.2f   \n",s,c);
+    
+    
+    return 0;
+}
